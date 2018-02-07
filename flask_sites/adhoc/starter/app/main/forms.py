7d7103702d4b16a682a3b7443a8ac3e5deacc1bf
mcdaded <daniel.r.mcdade@gmail.com>
2018-02-07 18:00:33 -0500

    new tutorial temp

--- flask_sites/adhoc/starter/app/main/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
-    SubmitField
-from flask_pagedown.fields import PageDownField
-
-
-class PostForm(FlaskForm):
-    body = PageDownField("What's on your mind?", validators=[DataRequired()])
-    submit = SubmitField('Submit')
