acc1ac09402bfb2568ddd52f382b269647c94080
mcdaded <daniel.r.mcdade@gmail.com>
2018-02-19 18:40:39 -0500

    keeping original js file

---
+++ flask_sites/adhoc/flask_react_d3/app/static/js/counterorig.js
@@ -0,0 +1,34 @@
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  tick() {
+    this.setState(prevState => ({
+      seconds: prevState.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      null,
+      "Seconds: ",
+      this.state.seconds
+    );
+  }
+}
+
+ReactDOM.render(
+  React.createElement(Timer, null),
+  document.getElementById('content')
+);
