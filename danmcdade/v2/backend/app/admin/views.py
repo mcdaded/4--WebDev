4b0b2e0739e9c84fca14c95c05e87e1e1a8f5a75
mcdaded <daniel.r.mcdade@gmail.com>
2018-07-08 22:46:33 -0400

    flask admin work

--- danmcdade/v2/backend/app/admin/views.py
+++ danmcdade/v2/backend/app/admin/views.py
@@ -1,10 +1,22 @@
-from flask import jsonify, request, current_app, url_for
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from . import admin
 from .. import db
 from ..models import User, Post
+from flask.ext.admin import BaseView, expose, ModelView
 
-app_admin = Admin(admin, name='admin', url_prefix='/admin', endpoint='admin')
-app_admin.add_view(ModelView(User, db.session))
-app_admin.add_view(ModelView(Post, db.session))
+
+class AdminView(BaseView):
+
+    @expose('/')
+    def index(self):
+        return self.render('admin/custom.html')
+
+    @expose('/second_page')
+    def second_page(self):
+        return self.render('admin/second_page.html')
+
+
+class AdminModelView(ModelView):
+    """ Model view for Flask models """
+    pass
