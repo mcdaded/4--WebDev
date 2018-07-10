82d0d3866c494870f255cefc86024ec972969617
mcdaded <daniel.r.mcdade@gmail.com>
2018-07-10 13:59:58 -0400

    in progress version with admin blueprint

--- danmcdade/v2/backend/app/admin/__init__.py
+++ danmcdade/v2/backend/app/admin/__init__.py
@@ -1,5 +1,8 @@
-from flask import Blueprint
+import flask_admin
+from ..models import User, Post, db
+from .admin_blueprint import AdminBlueprint
+from .views import AdminModelView
 
-admin = Blueprint('admin', __name__)
-
-from . import views
+# admin = AdminBlueprint('admin', __name__, url_prefix='/admin', static_folder='static', static_url_path='/static/admin')
+admin = AdminBlueprint('admin', __name__)
+admin.add_view(AdminModelView(User, db.session))
