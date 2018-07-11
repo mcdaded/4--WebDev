38d9d25f7963a0b4b487d3ffc33bd5c9bbf6755f
mcdaded <daniel.r.mcdade@gmail.com>
2018-07-11 07:59:41 -0400

    admin app work

--- danmcdade/v2/backend/app/admin/views.py
+++ danmcdade/v2/backend/app/admin/views.py
@@ -1,3 +1,4 @@
+from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from flask_admin import BaseView, expose
 from flask_admin.contrib.sqla import ModelView
@@ -7,18 +8,25 @@ from ..models import User, Post
 
 class AdminView(BaseView):
     @expose('/')
+    @login_required
     def index(self):
         return self.render('admin/index.html')
 
     def is_accessible(self):
-        return current_user.is_authenticated
+        return current_user.is_authenticated and current_user.is_administrator()
 
     def inaccessible_callback(self, name, **kwargs):
         # redirect to login page if user doesn't have access
-        return redirect(url_for('login', next=request.url))
+        flash('You do not have access to that page')
+        return redirect(url_for('auth.unconfirmed'))
 
 
 class AdminModelView(ModelView):
     """ Model view for Flask models """
     def is_accessible(self):
-        return current_user.is_authenticated
+        return current_user.is_authenticated and current_user.is_administrator()
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        flash('You do not have access to that page')
+        return redirect(url_for('auth.unconfirmed'))
