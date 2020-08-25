b1d88d9edbea327655255bfdcc48e82de00b188f
Dan <daniel.r.mcdade@gmail.com>
2020-08-25 17:07:55 -0400

    final commit

--- flask_sites/adhoc/starter/app/__init__.py
+++ flask_sites/adhoc/starter/app/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from config import config
@@ -19,6 +20,9 @@ moment = Moment()
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -28,10 +32,20 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     csrf.init_app(app)
     # attach routes and custom error pages here
+    # if app.config['SSL_REDIRECT']:
+    #     from flask_sslify import SSLify
+    #     sslify = SSLify(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     return app
